aa994dbc29c6252b25a43febf729109e97624138
Johan Hubau <johan.hubau@gmail.com>
2019-05-03 19:35:22 +0200

    Add CRUD behaviour
    Signed-off-by: Johan Hubau <johan.hubau@gmail.com>

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -6,6 +6,8 @@ use App\Event;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
 class EventController extends Controller
 {
@@ -13,15 +15,13 @@ class EventController extends Controller
      * Display a listing of the resource.
      *
      * @param Request $request
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     * @return AnonymousResourceCollection
      */
     public function index(Request $request)
     {
         if (!$request->has(['start', 'end'])) {
             return response(["message" => "Invalid request parameters."], 400);
         }
-//        $start = date("Y-m-d H:i:s", strtotime("2019-05-11 00:00:00"));
-//        $end = date("Y-m-d H:i:s", strtotime("2019-05-12 00:00:00"));
 
         $start = $request->get("start");
         $end = $request->get("end");
@@ -32,7 +32,7 @@ class EventController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -42,12 +42,28 @@ class EventController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
-        //
+        // TODO: Make sure the user can create the event for the specified association
+
+        $validated = $request->validate([
+            'title' => 'required|min:2|max:255',
+            'asso_id' => 'required|exists:associations,id',
+            'all_day' => 'boolean',
+            'start' => 'required|date',
+            'end' => 'required|date|after:start',
+            'url' => 'url|nullable',
+            'source' => 'nullable|exists:events,id'
+        ]);
+
+        $validated['start'] = date('Y-m-d H:i:s', strtotime($validated['start']));
+        $validated['end'] = date('Y-m-d H:i:s', strtotime($validated['end']));
+
+        Event::create($validated);
+        // returns a 200 OK status after successful creation with no body
     }
 
     /**
@@ -65,7 +81,7 @@ class EventController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit(Event $event)
     {
@@ -75,23 +91,47 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
+     * @param  Request $request
+     * @param  Event $event
+     * @return Response
      */
     public function update(Request $request, Event $event)
     {
-        //
+        // TODO: Make sure the event is editable
+
+        $validated = $request->validate([
+            'title' => 'min:2|max:255',
+            'asso_id' => 'exists:associations,id',
+            'all_day' => 'boolean',
+            'start' => 'date',
+            'end' => 'date|after:start',
+            'url' => 'url|nullable',
+            'source' => 'nullable|exists:events,id'
+        ]);
+
+        if ($request->has('start')) {
+            $validated['start'] = date('Y-m-d H:i:s', strtotime($validated['start']));
+        }
+        if ($request->has('end')) {
+            $validated['end'] = date('Y-m-d H:i:s', strtotime($validated['end']));
+        }
+
+        $event->update($validated);
+        // returns a 200 OK status after successful update with no body
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
+     * @param Event $event
+     * @return Response
+     * @throws \Exception
      */
     public function destroy(Event $event)
     {
-        //
+        // TODO: Make sure the event is editable
+
+        $event->delete();
+        // returns a 200 OK status after successful destruction with no body
     }
 }
